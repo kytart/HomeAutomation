c22f447a02e333e449f2b9f048f5301e2e7d2033
Michal Artazov <michal@artazov.cz>
2020-01-20 19:39:56 +0100

    add all other api endpoints for the client

--- src/Thermostat.ts
+++ src/Thermostat.ts
@@ -2,12 +2,55 @@ import * as Debug from 'debug';
 
 const debug = Debug('HomeAutomation:Thermostat');
 
+export enum Mode { DAY, NIGHT }
+
 export default class Thermostat {
 
 	private temperature: number = 20;
+	private desiredDayTemperature: number = 20;
+	private desiredNightTemperature: number = 20;
+	private currentMode: Mode = Mode.DAY;
+
+	public getTemperature() {
+		return this.temperature;
+	}
 
 	public setTemperature(temperature: number) {
 		this.temperature = temperature;
 		debug('set temperature: ' + temperature);
+	}
+
+	public getDesiredTemperature(mode: Mode) {
+		switch (mode) {
+			case Mode.DAY:
+				return this.desiredDayTemperature;
+			case Mode.NIGHT:
+				return this.desiredNightTemperature;
+			default:
+				throw new Error('invalid mode');
+		}
+	}
+
+	public setDesiredDayTemperature(mode: Mode, desiredTemperature: number) {
+		debug(`set desured day temperature; mode: ${Mode[mode]}, desiredTemperature: ${desiredTemperature}`);
+		switch (mode) {
+			case Mode.DAY:
+				this.desiredDayTemperature = desiredTemperature;
+				break;
+			case Mode.NIGHT:
+				this.desiredNightTemperature = desiredTemperature;
+				break;
+			default:
+				throw new Error('invalid mode');
+		}
+	}
+
+	public getMode() {
+		return this.currentMode;
+	}
+
+	public setMode(mode: Mode) {
+		debug(`set mode: ${Mode[mode]}`);
+		this.currentMode = mode;
 	}
 }
--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,6 @@
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
-import Thermostat from './Thermostat';
+import Thermostat, { Mode } from './Thermostat';
 
 export function route(
 	app: express.Application,
@@ -10,12 +10,55 @@ export function route(
 	app.use(bodyParser.json());
 	app.use(bodyParser.urlencoded({ extended: true }));
 
+	app.get('/temperature', (request: express.Request, response: express.Response) => {
+		const temperature = thermostat.getTemperature();
+		response.send({ temperature });
+	});
+
 	app.post('/temperature', (request: express.Request, response: express.Response) => {
 		const { temperature } = request.body;
 		if (typeof temperature === 'undefined') {
 			response.sendStatus(400);
 		} else {
 			thermostat.setTemperature(temperature);
+			response.sendStatus(200);
+		}
+	});
+
+	app.get('/mode', (request: express.Request, response: express.Response) => {
+		const mode = thermostat.getMode();
+		response.send({ mode: Mode[mode] });
+	});
+
+	app.post('/mode', (request: express.Request, response: express.Response) => {
+		const { mode: modeKey } = request.body;
+		const mode = Mode[modeKey as keyof typeof Mode];
+		if (typeof mode === 'undefined') {
+			response.sendStatus(400);
+		} else {
+			thermostat.setMode(mode);
+			response.sendStatus(200);
+		}
+	});
+
+	app.get('/desired-temperature', (request: express.Request, response: express.Response) => {
+		const { mode: modeKey } = request.query;
+		const mode = Mode[modeKey as keyof typeof Mode];
+		if (typeof mode === 'undefined') {
+			response.sendStatus(400);
+		} else {
+			const desiredTemperature = thermostat.getDesiredTemperature(mode);
+			response.send({ desiredTemperature });
+		}
+	});
+
+	app.post('/desired-temperature', (request: express.Request, response: express.Response) => {
+		const { mode: modeKey, desiredTemperature } = request.body;
+		const mode = Mode[modeKey as keyof typeof Mode];
+		if (typeof mode === 'undefined') {
+			response.sendStatus(400);
+		} else {
+			thermostat.setDesiredDayTemperature(mode, desiredTemperature);
 			response.sendStatus(200);
 		}
 	});
